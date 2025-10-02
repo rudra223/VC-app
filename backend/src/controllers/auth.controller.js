313b8477bcb1e4bfed5d9c6e89699dc2c54ba64a
Rudra Saini <rudrasaini78@gmail.com>
2025-10-02 16:46:41 +0530

    first commit of vc app

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,76 @@
+import User from "../models/User,.js"
+import jwt from "jsonwebtoken"
+
+export async function signup(req,res){
+    res.send("signup route");
+
+    const {email, password, fullName}=req.body;
+
+    try{
+        if(!email || !password || !fullName){
+            return res.status(400).json({message: "All fields are required"})
+        }
+
+        if(password.length<6){
+            return res.status(400).json({message:"Password must be at least 6 Characters"})
+        }
+        //checking for email format
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+        if (!emailRegex.test(email)) {
+            return res.status(400).json({ message: "Invalid email format" });
+        }
+
+        //checking if user already exist with this email
+
+        const existingUser=await User.findOne({email})
+
+        if(existingUser){
+            return res.status(400).json({message: "Email already exists , please use a diffrent email"})
+        }
+
+        const idx=Math.floor(Math.random()*100)+1 //random number for avatar api
+
+        const randomAvatar=`https://avatar.iran.liara.run/public/${idx}.png`;
+
+        const newUser=new User.create({
+            email,
+            password,
+            fullName,
+            profilePic: randomAvatar
+        })
+
+
+        //CREATE USER IN STREAM
+
+
+
+        //creating a json web token
+
+        const token=jwt.sign({userId:newUser._id},process.env.JWT_SECRET_KEY, {expiresIn: '7d'})
+
+        res.cookie("jwt",token,
+            {
+                maxage: 7*24*60*60*1000,
+                httpOnly:true,
+                sameSite:"strict",
+                secure: process.env.NODE_ENV==="production"
+        })
+
+        res.status(201).json({success:true, user:newUser})
+
+    }
+    catch(error){
+        console.log("error in signup controller");
+        res.status(500).json({message: "internal server error"})
+        
+    }
+}
+
+export function login(req,res){
+    res.send("login route");
+}
+
+export function logout(req,res){
+    res.send("logout route");
+}
